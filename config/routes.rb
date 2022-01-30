7e69c40e869c9b517ab463fe3b559f716c04e00a
Himanshu chouhan <himanshu.chouhan@anchanto.com>
2022-01-31 01:00:31 +0530

    Commit after alll styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'table', to:'pages#table'
-  resources :articles , only: [:show, :index, :new, :create, :edit, :update]
+  get 'about', to: 'pages#about'
+  resources :articles 
   #get 'home', to:'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
