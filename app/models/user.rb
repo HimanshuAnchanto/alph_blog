4fb617d1f6c67301c90f90833a805220fc0ab777
Himanshu chouhan <himanshu.chouhan@anchanto.com>
2022-02-05 00:54:14 +0530

    Add Validations to the user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ApplicationRecord
     has_many :articles
+    validates :username, presence: true,
+                         length: {minimum: 3, maximum: 12},
+                         uniqueness: { case_sensitive: false}
+    VALID_EMAIL_REGEX= /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, presence: true,
+                      uniqueness: {case_sensitive: false},
+                      length: {minimum: 5, maximum: 50},
+                      format: {with: VALID_EMAIL_REGEX}
 end
